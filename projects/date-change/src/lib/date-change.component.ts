f205355d47826804f0b4a5fc3942c41edfcd241f
Waleed OMEGA <waleedomega@hotmail.com>
2024-10-13 09:55:44 +0300

    lib init

---
+++ projects/date-change/src/lib/date-change.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-date-change',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      date-change works!
+    </p>
+  `,
+  styles: ``
+})
+export class DateChangeComponent {
+
+}
---
+++ projects/date-change/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of date-change
+ */
+
+export * from './lib/date-change.service';
+export * from './lib/date-change.component';
